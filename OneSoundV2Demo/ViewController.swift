7be3480ecbea0fd069f982d709eeb03af8aba1d8
SchoonSauce <adamschoonmaker12@gmail.com>
2015-03-01 15:46:00 -0500

    initial commit from local git

---
+++ OneSoundV2Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OneSoundV2Demo
+//
+//  Created by adam on 3/1/15.
+//  Copyright (c) 2015 Adam Schoonmaker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
